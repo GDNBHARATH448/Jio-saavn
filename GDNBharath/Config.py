5918844eaec499e9781672562c3e072899dce379
GDNBHARATH448 <gollapallibharath24@gmail.com>
2024-12-30 10:14:15 +0530

    Create Config.py

---
+++ GDNBharath/Config.py
@@ -0,0 +1,8 @@
+# config.py
+
+class JioSaavnConfig:
+    BASE_URL = 'https://www.jiosaavn.com'
+    SEARCH_URL = BASE_URL + '/search/{query}'
+    USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36'
+    
+    # Add any other configurations you may need
