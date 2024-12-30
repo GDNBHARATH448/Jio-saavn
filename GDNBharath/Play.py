67c9ea515c635938bdcf605ec5e9675ed670d40a
GDNBHARATH448 <gollapallibharath24@gmail.com>
2024-12-30 11:00:29 +0530

    Create Play.py

---
+++ GDNBharath/Play.py
@@ -0,0 +1,54 @@
+{
+  "albums": [
+    {
+      "album_id": "1",
+      "title": "Album Title",
+      "artist": "Artist Name",
+      "release_date": "2024-01-01",
+      "cover_image": "https://example.com/cover.jpg",
+      "songs": [
+        {
+          "song_id": "101",
+          "title": "Song Title 1",
+          "duration": "3:45",
+          "audio_url": "https://example.com/song1.mp3"
+        },
+        {
+          "song_id": "102",
+          "title": "Song Title 2",
+          "duration": "4:20",
+          "audio_url": "https://example.com/song2.mp3"
+        }
+      ]
+    }
+  ],
+  "artists": [
+    {
+      "artist_id": "1",
+      "name": "Artist Name",
+      "bio": "Artist biography goes here.",
+      "image_url": "https://example.com/artist.jpg"
+    }
+  ],
+  "playlists": [
+    {
+      "playlist_id": "1",
+      "name": "Playlist Name",
+      "description": "Playlist description goes here.",
+      "songs": [
+        {
+          "song_id": "101",
+          "title": "Song Title 1",
+          "duration": "3:45",
+          "audio_url": "https://example.com/song1.mp3"
+        },
+        {
+          "song_id": "103",
+          "title": "Song Title 3",
+          "duration": "2:30",
+          "audio_url": "https://example.com/song3.mp3"
+        }
+      ]
+    }
+  ]
+}
