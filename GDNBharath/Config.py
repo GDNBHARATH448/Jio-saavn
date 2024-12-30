a69f001635c49537374220205fae173a240577d5
GDNBHARATH448 <gollapallibharath24@gmail.com>
2024-12-30 13:05:27 +0530

    Update Config.py

--- GDNBharath/Config.py
+++ GDNBharath/Config.py
@@ -1,3 +1,6 @@
+import os
+from dotenv import load_dotenv
+
 # config.py
 
 class JioSaavnConfig:
@@ -31,3 +34,20 @@ import logging
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Get environment variables with default values
+START_IMG_URL = os.getenv("START_IMG_URL", "https://default.example.com/start.jpg")
+PING_IMG_URL = os.getenv("PING_IMG_URL", "https://default.example.com/ping.jpg")
+PLAYLIST_IMG_URL = os.getenv("PLAYLIST_IMG_URL", "https://default.example.com/playlist.jpg")
+STATS_IMG_URL = os.getenv("STATS_IMG_URL", "https://default.example.com/stats.jpg")
+TELEGRAM_AUDIO_URL = os.getenv("TELEGRAM_AUDIO_URL", "https://default.example.com/telegram_audio.jpg")
+TELEGRAM_VIDEO_URL = os.getenv("TELEGRAM_VIDEO_URL", "https://default.example.com/telegram_video.jpg")
+STREAM_IMG_URL = os.getenv("STREAM_IMG_URL", "https://default.example.com/stream.jpg")
+SOUNCLOUD_IMG_URL = os.getenv("SOUNCLOUD_IMG_URL", "https://default.example.com/soundcloud.jpg")
+JIOSAAVN_IMG_URL = os.getenv("JIOSAAVN_IMG_URL", "https://default.example.com/jiosaavn.jpg")
+SPOTIFY_ARTIST_IMG_URL = os.getenv("SPOTIFY_ARTIST_IMG_URL", "https://default.example.com/spotify_artist.jpg")
+SPOTIFY_ALBUM_IMG_URL = os.getenv("SPOTIFY_ALBUM_IMG_URL", "https://default.example.com/spotify_album.jpg")
+SPOTIFY_PLAYLIST_IMG_URL = os.getenv("SPOTIFY_PLAYLIST_IMG_URL", "https://default.example.com/spotify_playlist.jpg")
